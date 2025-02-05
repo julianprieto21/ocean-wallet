0748135d04436ad11eac895a5dc7d73e7a4c165e
Julian Prieto <prietojulian2003@gmail.com>
2025-02-04 22:13:38 -0300

    add basic google login

---
+++ app/[lang]/login/layout.tsx
@@ -0,0 +1,28 @@
+import { Kanit } from "next/font/google";
+import "../globals.css";
+import { Locale } from "@/i18n/routing";
+
+const kanit = Kanit({
+  variable: "--font-kanit",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export default async function LoginLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ lang: Locale }>;
+}>) {
+  const { lang } = await params;
+  return (
+    <html lang={lang}>
+      <body className={`${kanit.className} antialiased`}>
+        <main className="size-full flex flex-row gap-8 relative">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/[lang]/login/page.tsx
@@ -0,0 +1,16 @@
+import { signIn } from "@/auth";
+
+export default function SignIn() {
+  return (
+    <div className="grid place-content-center size-full">
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit">Sign in with Google</button>
+      </form>
+    </div>
+  );
+}
