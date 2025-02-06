2ac5e48b90750c04664210f5829c68f72fb89fdf
Julian Prieto <prietojulian2003@gmail.com>
2025-02-06 20:17:58 -0300

    add types.ts

---
+++ lib/types.ts
@@ -0,0 +1,31 @@
+export type AccountType = "general" | "crypto";
+
+export type Account = {
+  account_id: string;
+  account_type: AccountType;
+  name: string;
+  hex_code: string;
+};
+
+export type Category =
+  | "house"
+  | "transport"
+  | "food"
+  | "health_beauty"
+  | "entertainment_leisure"
+  | "savings_investments";
+
+export type TransactionType = "income" | "expense";
+
+export type Transaction = {
+  transaction_id: string;
+  transaction_description: string;
+  transaction_category: Category;
+  transaction_subcategory: string;
+  transaction_type: TransactionType;
+  created_at: string;
+  amount: number;
+  currency_id: string;
+  transfer_id: string;
+  quota_id: string;
+};
