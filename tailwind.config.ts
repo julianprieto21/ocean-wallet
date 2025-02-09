b74c47d56d9fb6b0495925d92ea02c1f3dd59c77
Julian Prieto <prietojulian2003@gmail.com>
2025-02-09 13:34:41 -0300

    add tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          50: "var(--primary-50)",
+          100: "var(--primary-100)",
+          200: "var(--primary-200)",
+          250: "var(--primary-250)",
+          300: "var(--primary-300)",
+          400: "var(--primary-400)",
+          500: "var(--primary-500)",
+          red: "var(--primary-red)",
+          green: "var(--primary-green)",
+        },
       },
     },
   },
