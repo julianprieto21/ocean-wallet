3332fbec80ceee06ae1e8e3b4bab65f9d68f21a3
Julian Prieto <prietojulian2003@gmail.com>
2025-02-10 18:42:57 -0300

    add wallet and crypto logos

--- middleware.ts
+++ middleware.ts
@@ -48,5 +48,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|icons).*)"],
 };
