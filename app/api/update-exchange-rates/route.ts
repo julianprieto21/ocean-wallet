821c301dd61265c6847241ee28fd900aeb60abd9
Julian Prieto <prietojulian2003@gmail.com>
2025-02-12 14:44:57 -0300

    fix action API

--- app/api/update-exchange-rates/route.ts
+++ app/api/update-exchange-rates/route.ts
@@ -18,9 +18,9 @@ export async function GET(req: Request) {
 
   const date = new Date();
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const lastUpdate = `${year}.${month}.${day}`;
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const lastUpdate = `${year}-${month}-${day}`;
 
   try {
     const { rows: currencies } = await pool.query(
