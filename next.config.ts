74061af00e0b496b61967507bb6f1a1539e5fbd0
Julian Prieto <prietojulian2003@gmail.com>
2025-02-16 21:20:52 -0300

    update dependencies and fix some warnings

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    optimizePackageImports: ["@mantine/core", "@mantine/charts"],
+  },
+};
 
 // export default withNextIntl(nextConfig);
 export default withNextIntl(nextConfig);
