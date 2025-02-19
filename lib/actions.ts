4bba69afeb5b9077ab0d104dba70ff4574d9f092
Julian Prieto <prietojulian2003@gmail.com>
2025-02-18 22:07:05 -0300

    add createTransaction action

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,7 +2,7 @@
 import { Pool } from "@neondatabase/serverless";
 import { auth } from "@/auth";
 import { revalidatePath } from "next/cache";
-import { accountSchema } from "./squemas";
+import { accountSchema, transactionSchema } from "./squemas";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -20,7 +20,7 @@ const verifySession = async () => {
 export async function createAccount(formData: FormData) {
   const user = await verifySession();
   try {
-    const { name, type, provider, initial } = accountSchema.parse(
+    const { name, type, provider, initial, currency_id } = accountSchema.parse(
       Object.fromEntries(formData)
     );
 
@@ -28,7 +28,8 @@ export async function createAccount(formData: FormData) {
       `INSERT INTO accounts (user_id, name, type, provider) VALUES ($1, $2, $3, $4) RETURNING *;`,
       [user.id, name, type, provider]
     );
-    if (initial == "0") return rows[0];
+    if (!initial) return rows[0];
+    const formatInitial = initial.slice(1).replace(/\,/g, "");
 
     await pool.query(
       `INSERT INTO transactions (account_id, description, type, category, amount, currency_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
@@ -37,14 +38,51 @@ export async function createAccount(formData: FormData) {
         "-",
         "income",
         "initial-balance",
-        initial,
-        user.preferenceCurrency,
+        formatInitial,
+        currency_id,
       ]
     );
     return rows[0];
   } catch (error) {
     console.log(error);
     throw new Error("Error creating account");
+  } finally {
+    revalidatePath("/");
+  }
+}
+
+export async function createTransaction(formData: FormData) {
+  await verifySession();
+  try {
+    const {
+      account_id,
+      description,
+      type,
+      category,
+      subcategory,
+      created_at,
+      amount,
+      currency_id,
+    } = transactionSchema.parse(Object.fromEntries(formData));
+    const formatAmount = amount.slice(1).replace(/\,/g, "");
+
+    const { rows } = await pool.query(
+      `INSERT INTO transactions (account_id, description, type, category, subcategory, amount, created_at, currency_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
+      [
+        account_id,
+        description,
+        type,
+        category,
+        subcategory ?? "",
+        formatAmount,
+        created_at,
+        currency_id,
+      ]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error creating transaction");
   } finally {
     revalidatePath("/");
   }
