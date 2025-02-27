323cc0b0f8d190fba9940a144caa12948fb53708
Julian Prieto <prietojulian2003@gmail.com>
2025-02-26 22:29:04 -0300

    added functionality to menu button

--- app/[locale]/(main)/components/Menu.tsx
+++ app/[locale]/(main)/components/Menu.tsx
@@ -1,9 +1,21 @@
+"use client";
+import { useModalStore } from "@/lib/store/modalStore";
 import { Menu as MenuIcon } from "lucide-react";
 
 export default function Menu() {
+  const { setModalOpen, setModalActive } = useModalStore((state) => state);
+  const OnClick = () => {
+    setModalActive("menu");
+    setModalOpen(true);
+  };
   return (
     <>
-      <button title="Menu" type="button" className="absolute right-0">
+      <button
+        title="Menu"
+        type="button"
+        className="absolute right-0"
+        onClick={OnClick}
+      >
         <MenuIcon className="size-8 flex-shrink-0 text-primary-300 hover:text-primary-400" />
       </button>
       <div className="absolute"></div>
